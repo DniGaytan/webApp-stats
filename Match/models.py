000dbc2cfd5e00757b2793e7f6c28983af2eadb4
Daniel Gaytan <dnigaytan07@gmail.com>
2019-12-08 06:38:06 -0600

    footer was added to the site, Match class was modified and irrelevant models were removed

--- Match/models.py
+++ Match/models.py
@@ -1,32 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-GAME_CHOICES = (
-    ('fifa', 'FIFA'),
-    ('mk', 'Mortal Kombat'),
-)
 
-
-class Tournament(models.Model):
-    name = models.CharField(max_length=50, default='No name')
-
-
-class TournamentParticipants(models.Model):
-    tournament = models.ForeignKey(
-        Tournament, on_delete=models.CASCADE, default=1)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
+GAME_CHOICES = [
+    ('FIFA', 'FIFA'),
+    ('SMASH', 'Smash'),
+]
 
 
 class Match(models.Model):
-    match_admin = models.ForeignKey(
-        User, related_name='admin', on_delete=None, default=1)
-    player_one = models.ForeignKey(
-        User, related_name='player_one', on_delete=None, default=1)
-    player_two = models.ForeignKey(
-        User, related_name='player_two', on_delete=None, default=1)
+    participants = models.ManyToManyField(User)
+    admin = models.ForeignKey(
+        User, related_name='admin', default=1, on_delete=None)
+    participant1Score = models.BigIntegerField()
+    participant2Score = models.BigIntegerField()
     game = models.CharField(
-        max_length=15, choices=GAME_CHOICES, default='fifa')
-    result_player_one = models.IntegerField(default=0)
-    result_player_two = models.IntegerField(default=0)
-    tournament = models.ForeignKey(
-        Tournament, on_delete=models.CASCADE, default=1)
+        max_length=10, choices=GAME_CHOICES, default='FIFA')
