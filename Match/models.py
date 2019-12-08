3ee4b0ee5eeb92f412d67b46a6cd296227f857a9
Daniel Gaytan <dnigaytan07@gmail.com>
2019-12-08 06:48:13 -0600

    migrations were added

--- Match/models.py
+++ Match/models.py
@@ -12,7 +12,7 @@ class Match(models.Model):
     participants = models.ManyToManyField(User)
     admin = models.ForeignKey(
         User, related_name='admin', default=1, on_delete=None)
-    participant1Score = models.BigIntegerField()
-    participant2Score = models.BigIntegerField()
+    participant1Score = models.BigIntegerField(default=1)
+    participant2Score = models.BigIntegerField(default=1)
     game = models.CharField(
         max_length=10, choices=GAME_CHOICES, default='FIFA')
